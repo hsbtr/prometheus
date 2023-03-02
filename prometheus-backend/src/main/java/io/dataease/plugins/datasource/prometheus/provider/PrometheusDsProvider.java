b7d4346a19fd52a434e88518fbf74cec5b37bd4f
huanglei <2936412130@qq.com>
2023-03-02 17:24:12 +0800

    feat: 完成prometheus查看指标和指标详情

--- prometheus-backend/src/main/java/io/dataease/plugins/datasource/prometheus/provider/PrometheusDsProvider.java
+++ prometheus-backend/src/main/java/io/dataease/plugins/datasource/prometheus/provider/PrometheusDsProvider.java
@@ -2,7 +2,6 @@ package io.dataease.plugins.datasource.prometheus.provider;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.convert.Convert;
-import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
@@ -14,11 +13,14 @@ import io.dataease.plugins.common.base.mapper.DeDriverMapper;
 import io.dataease.plugins.common.constants.DatasourceTypes;
 import io.dataease.plugins.common.dto.datasource.TableDesc;
 import io.dataease.plugins.common.dto.datasource.TableField;
-import io.dataease.plugins.common.exception.DataEaseException;
 import io.dataease.plugins.common.request.datasource.DatasourceRequest;
 import io.dataease.plugins.datasource.entity.JdbcConfiguration;
 import io.dataease.plugins.datasource.prometheus.engine.okhttp.HttpClientConfig;
 import io.dataease.plugins.datasource.prometheus.engine.okhttp.HttpClientUtils;
+import io.dataease.plugins.datasource.prometheus.engine.prometheus.datasource.MetricsInfoData;
+import io.dataease.plugins.datasource.prometheus.engine.prometheus.datasource.MetricsInfoResp;
+import io.dataease.plugins.datasource.prometheus.engine.prometheus.datasource.MetricsInfoResult;
+import io.dataease.plugins.datasource.prometheus.engine.prometheus.datasource.MetricsResp;
 import io.dataease.plugins.datasource.provider.DefaultJdbcProvider;
 import io.dataease.plugins.datasource.provider.ExtendedJdbcClassLoader;
 import org.apache.commons.lang3.StringUtils;
@@ -26,11 +28,10 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 @Component()
@@ -125,108 +126,137 @@ public class PrometheusDsProvider extends DefaultJdbcProvider {
      * 获取表名称
      */
     @Override
-    public List<TableDesc> getTables(DatasourceRequest datasourceRequest) throws Exception {
+    public List<TableDesc> getTables(DatasourceRequest datasourceRequest){
         List<TableDesc> tables = new ArrayList<>();
-        String queryStr = getTablesSql(datasourceRequest);
-        JdbcConfiguration jdbcConfiguration =
-                new Gson().fromJson(datasourceRequest.getDatasource().getConfiguration(), JdbcConfiguration.class);
-        int queryTimeout = Math.max(jdbcConfiguration.getQueryTimeout(), 0);
-        try (Connection con = getConnectionFromPool(datasourceRequest); Statement statement = getStatement(con,
-                queryTimeout); ResultSet resultSet = statement.executeQuery(queryStr)) {
-            while (resultSet.next()) {
-                tables.add(getTableDesc(datasourceRequest, resultSet));
-            }
-        } catch (Exception e) {
-            DataEaseException.throwException(e);
+
+        if (ObjectUtil.isNull(datasourceRequest)){
+            return tables;
+        }
+
+        Datasource datasource = datasourceRequest.getDatasource();
+        if (ObjectUtil.isNull(datasource)){
+            return tables;
+        }
+
+        String configuration = datasource.getConfiguration();
+        if (StrUtil.isBlank(configuration)){
+            return tables;
+        }
+
+        HttpClientConfig config = new HttpClientConfig();
+
+        Map<String,Object> maps = (Map<String,Object>) JSON.parse(configuration);
+        String headData = getConnectionParam(config, maps,"/api/v1/label/__name__/values");
+
+        String result;
+        if (StrUtil.isNotBlank(headData)){
+            result = HttpClientUtils.doGet(config, headData);
+        }else {
+           result = HttpClientUtils.doGet(config);
+        }
+
+        if (StrUtil.isBlank(result)){
+            return tables;
+        }
+
+        MetricsResp metricsResp = JSONUtil.toBean(result, MetricsResp.class);
+
+        if (metricsResp.getStatus().equals("success") && CollUtil.isNotEmpty(metricsResp.getData())){
+            tables = metricsResp.getData().stream().filter(Objects::nonNull).map(s -> {
+                TableDesc desc = new TableDesc();
+                desc.setName(s);
+                return desc;
+            }).collect(Collectors.toList());
         }
 
         return tables;
     }
 
     /**
-     * 获取表名称
-     */
-    private TableDesc getTableDesc(DatasourceRequest datasourceRequest, ResultSet resultSet) throws SQLException {
-        TableDesc tableDesc = new TableDesc();
-        tableDesc.setName(resultSet.getString(1));
-        return tableDesc;
-    }
-
-    /**
      * 获取表字段信息
      */
     @Override
-    public List<TableField> getTableFields(DatasourceRequest datasourceRequest) throws Exception {
+    public List<TableField> getTableFields(DatasourceRequest datasourceRequest){
         List<TableField> list = new LinkedList<>();
-        try (Connection connection = getConnectionFromPool(datasourceRequest)) {
-            DatabaseMetaData databaseMetaData = connection.getMetaData();
-            ResultSet resultSet = databaseMetaData.getColumns(null, null, datasourceRequest.getTable(), "%");
-            while (resultSet.next()) {
-                String tableName = resultSet.getString("TABLE_NAME").toUpperCase();
-                String database;
-                database = resultSet.getString("TABLE_CAT");
-                if (database != null) {
-                    if (tableName.equals(datasourceRequest.getTable()) && database.equalsIgnoreCase(getDatabase(datasourceRequest))) {
-                        TableField tableField = getTableFiled(resultSet, datasourceRequest);
-                        list.add(tableField);
-                    }
-                } else {
-                    if (tableName.equals(datasourceRequest.getTable())) {
-                        TableField tableField = getTableFiled(resultSet, datasourceRequest);
-                        list.add(tableField);
+
+        if (ObjectUtil.isNull(datasourceRequest)){
+            return list;
+        }
+
+        Datasource datasource = datasourceRequest.getDatasource();
+        if (ObjectUtil.isNull(datasource)){
+            return list;
+        }
+
+        String table = datasourceRequest.getTable();
+        if (StrUtil.isBlank(table)){
+            return list;
+        }
+
+        String configuration = datasource.getConfiguration();
+        if (StrUtil.isBlank(configuration)){
+            return list;
+        }
+
+        HttpClientConfig config = new HttpClientConfig();
+        Map<String,Object> maps = (Map<String,Object>) JSON.parse(configuration);
+        long time = System.currentTimeMillis() / 1000;
+        String path = "/api/v1/query?query="+ table + "&time=" + time;
+        String headData = getConnectionParam(config, maps,path);
+
+
+        String result;
+        if (StrUtil.isNotBlank(headData)){
+            result = HttpClientUtils.doGet(config, headData);
+        }else {
+            result = HttpClientUtils.doGet(config);
+        }
+
+        if (StrUtil.isBlank(result)){
+            return list;
+        }
+
+        MetricsInfoResp metricsInfoResp = JSONUtil.toBean(result, MetricsInfoResp.class);
+
+        if (ObjectUtil.isNotNull(metricsInfoResp) && metricsInfoResp.getStatus().equals("success")){
+            MetricsInfoData data = metricsInfoResp.getData();
+            if (ObjectUtil.isNotNull(data)){
+                if (CollUtil.isNotEmpty(data.getResult()) && data.getResult().size() >= 1){
+                    MetricsInfoResult metricsInfoResult = data.getResult().get(0);
+                    Map<String, Object> metric = metricsInfoResult.getMetric();
+                    if (CollUtil.isNotEmpty(metric)){
+                        metric.forEach((key,value) ->{
+                            TableField tableField = new TableField();
+                            tableField.setFieldName(key);
+                            if (value.getClass().equals(String.class)){
+                                tableField.setFieldType("STRING");
+                            }
+                            if (value.getClass().equals(Integer.class)){
+                                tableField.setFieldType("INTEGER");
+                            }
+                            if (value.getClass().equals(Double.class)){
+                                tableField.setFieldType("DOUBLE");
+                            }
+                            if (value.getClass().equals(Float.class)){
+                                tableField.setFieldType("FLOAT");
+                            }
+                            if (value.getClass().equals(Long.class)){
+                                tableField.setFieldType("LONG");
+                            }
+                            if (value.getClass().equals(Character.class)){
+                                tableField.setFieldType("CHARACTER");
+                            }
+                            if (value.getClass().equals(Boolean.class)){
+                                tableField.setFieldType("BOOLEAN");
+                            }
+                            list.add(tableField);
+                        });
                     }
                 }
             }
-            resultSet.close();
-        } catch (SQLException e) {
-            DataEaseException.throwException(e);
-        } catch (Exception e) {
-            DataEaseException.throwException("Data source connection exception: " + e.getMessage());
         }
+
         return list;
-    }
-
-    /**
-     * 获取数据源
-     */
-    private String getDatabase(DatasourceRequest datasourceRequest) {
-        JdbcConfiguration jdbcConfiguration =
-                new Gson().fromJson(datasourceRequest.getDatasource().getConfiguration(), JdbcConfiguration.class);
-        return jdbcConfiguration.getDataBase();
-    }
-
-    /**
-     * 获取表字段
-     */
-    private TableField getTableFiled(ResultSet resultSet, DatasourceRequest datasourceRequest) throws SQLException {
-        TableField tableField = new TableField();
-        String colName = resultSet.getString("COLUMN_NAME");
-        tableField.setFieldName(colName);
-        String remarks = resultSet.getString("REMARKS");
-        if (remarks == null || remarks.equals("")) {
-            remarks = colName;
-        }
-        tableField.setRemarks(remarks);
-        String dbType = resultSet.getString("TYPE_NAME").toUpperCase();
-        tableField.setFieldType(dbType);
-        if (dbType.equalsIgnoreCase("LONG")) {
-            tableField.setFieldSize(65533);
-        }
-        if (StringUtils.isNotEmpty(dbType) && dbType.toLowerCase().contains("date") && tableField.getFieldSize() < 50) {
-            tableField.setFieldSize(50);
-        }
-
-        if (datasourceRequest.getDatasource().getType().equalsIgnoreCase(DatasourceTypes.hive.name()) && tableField.getFieldType().equalsIgnoreCase("BOOLEAN")) {
-            tableField.setFieldSize(1);
-        } else {
-            String size = resultSet.getString("COLUMN_SIZE");
-            if (size == null) {
-                tableField.setFieldSize(1);
-            } else {
-                tableField.setFieldSize(Integer.valueOf(size));
-            }
-        }
-        return tableField;
     }
 
     /**
@@ -252,7 +282,7 @@ public class PrometheusDsProvider extends DefaultJdbcProvider {
         HttpClientConfig config = new HttpClientConfig();
 
         Map<String,Object> maps = (Map<String,Object>) JSON.parse(configuration);
-        String headData = getConnectionParam(config, maps);
+        String headData = getConnectionParam(config, maps,"/metrics");
 
 
         String resultData;
@@ -269,7 +299,7 @@ public class PrometheusDsProvider extends DefaultJdbcProvider {
         }
     }
 
-    private String getConnectionParam(HttpClientConfig config, Map<String, Object> maps) {
+    private String getConnectionParam(HttpClientConfig config, Map<String, Object> maps,String path) {
         String headData = null;
         if (CollUtil.isNotEmpty(maps) && maps.containsKey("host") && maps.containsKey("port")){
             String host = null;
@@ -294,10 +324,10 @@ public class PrometheusDsProvider extends DefaultJdbcProvider {
 
             if (StrUtil.isNotBlank(host) && ObjectUtil.isNotNull(port)){
                 if (host.startsWith("http://")){
-                    String url = host + ":" + port + "/metrics";
+                    String url = host + ":" + port + path;
                     config.setMasterUrl(url);
                 }else {
-                    String url = "http://" + host + ":" + port + "/metrics";
+                    String url = "http://" + host + ":" + port + path;
                     config.setMasterUrl(url);
                 }
             }
